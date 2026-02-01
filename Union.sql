306e8a57a5661d7bd480258a136deda3fd67e9ff
mala9311 <165881518+mala9311@users.noreply.github.com>
2026-02-01 18:12:42 +0530

    Union

---
+++ Union.sql
@@ -0,0 +1,36 @@
+-- UNION 
+
+SELECT age , gender     # NOT a correct way to combine age ,gender with first and last name
+FROM employee_demographics
+UNION
+SELECT first_name , last_name
+FROM employee_salary
+;
+
+SELECT first_name , last_name 
+FROM employee_demographics
+UNION All    # WE CAN ALSO WRITE 'DISTINCT' FOR all NON REPEATING NAME
+SELECT first_name , last_name
+FROM employee_salary;
+
+SELECT first_name , last_name , 'Old' AS Label
+FROM employee_demographics
+WHERE age > 40
+UNION 
+SELECT first_name , last_name , 'Old' AS Label
+FROM employee_salary
+WHERE salary > 70000 ;
+
+SELECT first_name , last_name , 'Old Man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION 
+SELECT first_name , last_name , 'Old Lady' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION 
+SELECT first_name , last_name , 'Highly Paid Employee' AS Label
+FROM employee_salary
+WHERE salary > 70000 
+ORDER BY first_name , last_name;
+
