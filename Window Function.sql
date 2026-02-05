b4ada6c40adf240c5b8c8de8f46d00a0b8172d5f
mala9311 <165881518+mala9311@users.noreply.github.com>
2026-02-05 16:02:37 +0530

    ROW() , RANK() , DENSE_RANK()

---
+++ Window Function.sql
@@ -0,0 +1,34 @@
+-- Window Function
+
+SELECT dem.first_name , dem.last_name , gender , SUM(salary) 
+OVER(PARTITION  BY gender)
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id 
+;
+
+SELECT dem.first_name , dem.last_name , gender , salary,
+SUM(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) AS Rolling_total
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id ;
+    
+SELECT dem.employee_id , dem.first_name , dem.last_name , gender , salary,
+ROW_NUMBER() OVER(PARTITION BY gender) AS row_no
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id ;
+    
+SELECT dem.employee_id , dem.first_name , dem.last_name , gender , salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_no
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id ;
+    
+SELECT dem.employee_id , dem.first_name , dem.last_name , gender , salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num,
+DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS dense_rank_num
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id ;
