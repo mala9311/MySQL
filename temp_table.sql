8c0673e0fae775f37f2285ee5714edb5f91c842d
mala9311 <165881518+mala9311@users.noreply.github.com>
2026-02-06 11:31:10 +0530

    temporary table

---
+++ temp_table.sql
@@ -0,0 +1,24 @@
+-- Temporary table
+
+CREATE TEMPORARY TABLE temp_table 
+(first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(100)
+);
+
+SELECT*
+FROM temp_table ;
+
+INSERT INTO temp_table
+VALUES('Mala','Kumari','Captain Jack Sparrow');
+
+SELECT*
+FROM temp_table ;
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+SELECT*
+FROM salary_over_50k;
+
