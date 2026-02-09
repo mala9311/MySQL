58784b12f1275450b4e78687afd46d53d18ec178
mala9311 <165881518+mala9311@users.noreply.github.com>
2026-02-09 12:39:50 +0530

    part02 delimiter and parameter

--- Stored procedure.sql
+++ Stored procedure.sql
@@ -18,3 +18,19 @@ BEGIN
 	WHERE salary >= 10000;
 END $$
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE larger_salaries4(p_employee_id INT)
+BEGIN
+	SELECT salary
+	FROM employee_salary
+	WHERE employee_id = p_employee_id;
+	
+END $$
+DELIMITER ;
+
+CALL larger_salaries4(1);
+
+
+
+
