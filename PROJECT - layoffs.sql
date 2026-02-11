81a8e8093762f6678603117adec13c5caf82cc31
mala9311 <165881518+mala9311@users.noreply.github.com>
2026-02-11 17:55:35 +0530

    Standardize and finding null and blank value

--- PROJECT - layoffs.sql
+++ PROJECT - layoffs.sql
@@ -74,3 +74,86 @@ SET SQL_SAFE_UPDATES = 1;
 
 SELECT * FROM layoffs_staging02
  WHERE row_num > 1;
+ 
+ -- Standardizing data
+ 
+ SELECT company , TRIM(company)
+ FROM layoffs_staging02 ;
+ 
+ UPDATE layoffs_staging02
+ SET company = TRIM(company);
+ 
+ SELECT DISTINCT industry
+ FROM layoffs_staging02
+ ORDER BY 1 ;
+ 
+ SELECT * 
+ FROM layoffs_staging02
+ WHERE industry LIKE 'crypto%';
+ 
+ UPDATE layoffs_staging02
+ SET  industry = 'crypto'
+ WHERE industry LIKE 'crypto%';
+ 
+ SELECT DISTINCT industry
+ FROM layoffs_staging02 ;
+ 
+ SELECT DISTINCT location
+ FROM layoffs_staging02 
+ ORDER BY 1;
+ 
+ SELECT DISTINCT country , TRIM(TRAILING '.' FROM country) 
+ FROM layoffs_staging02
+ ORDER BY 1 ;
+ UPDATE layoffs_staging02
+ SET country = TRIM(TRAILING '.' FROM country) 
+ WHERE country LIKE 'United States%';
+ 
+ SELECT *
+ FROM layoffs_staging02 ;
+ 
+ SELECT `date` ,
+ STR_TO_DATE(`date` , '%m/%d/%Y')
+ FROM layoffs_staging02 ;
+ 
+ SELECT `date`
+FROM layoffs_staging02
+WHERE STR_TO_DATE(`date`, '%m-%d-%Y') IS NULL;
+
+ UPDATE layoffs_staging02
+ SET `date` =  STR_TO_DATE(`date` , '%m-%d-%Y') ;  ## This can only be written if we have date in same format either in '/' form or '-'
+ 
+ UPDATE layoffs_staging02
+ SET `date` = 
+		CASE 
+        WHEN `date` LIKE '%/%' THEN STR_TO_DATE (`date` , '%m/%d/%Y') 
+        WHEN  `date` LIKE '%-%' THEN STR_TO_DATE(`date` , '%m-%d-%Y')
+        END;
+
+ALTER TABLE layoffs_staging02
+MODIFY COLUMN `date` DATE ;
+
+SELECT *
+FROM layoffs_staging02 ;
+
+-- Null values and Blank values
+
+SELECT * 
+FROM layoffs_staging02
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL ;
+
+SELECT *
+FROM layoffs_staging02
+WHERE industry IS NULL 
+OR industry = '';
+SELECT * 
+FROM layoffs_staging02
+WHERE company = 'Airbnb' ;
+
+SELECT *
+FROM layoffs_staging02 t1
+JOIN layoffs_staging02 t2
+	ON t1.company =t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
